aa0234d56d51c9fd272b45a79fd2204d329b3e05
Kushal Pradhan <kushalpradhan127@gmail.com>
2026-03-17 18:04:56 +0530

    creatd atlas database

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://127.0.0.1:27017/mern_admin";
+// const URI = " mongodb://127.0.0.1:27017/mern_admin";
+const URI = process.env.MONGODB_URI;
 // mongoose.connect(URI);
 
 const connectDb = async () => {
   try {
     await mongoose.connect(URI);
-    console.log("Connection sucessful to DB");
+    console.log("Connection successful to DB");
   } catch (error) {
     console.error("database connection failed");
     process.exit(0);
